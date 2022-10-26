61ecaa99e9f24f2ba0ed7e98f6a48f5083811b99
michaelkot97 <michael.kot97@gmail.com>
2022-10-26 17:12:49 +0300

    Added CI structure

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-conftest test --policy conftest/ manifests/
-yamllint manifests/ -d yamllint/conf.yaml
-yamllint demo-chart/templates/ -d yamllint/conf.yaml
+conftest test --policy ci/conftest/ manifests/
+yamllint manifests/ -d ci/yamllint/conf.yaml
+yamllint demo-chart/templates/ -d ci/yamllint/conf.yaml
+ct lint --lint-conf ci/chart-testing/lintconf.yaml --chart-yaml-schema ci/chart-testing/chart_schema.yaml
